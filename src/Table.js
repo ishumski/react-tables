272bd25460985dbf2818cf1c8f091f412c505440
ishumski <whalterwhite23@gmail.com>
2021-05-31 12:45:08 +0300

    Add pagination

--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { useTable, useSortBy } from 'react-table'
+import { useTable, useSortBy, usePagination } from 'react-table'
 
 const Table = ({ data }) => {
   const columns = useMemo(
@@ -16,40 +16,99 @@ const Table = ({ data }) => {
     []
   );
 
-  const { rows, headerGroups, prepareRow } = useTable({ columns, data }, useSortBy);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    prepareRow,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageIndex: 0 }
+    },
+    useSortBy,
+    usePagination);
 
-  return (<table>
-    <thead>
-      {headerGroups.map(headerGroup => {
-        return <tr>
-          {headerGroup.headers.map(header => {
+
+  return (
+    <>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => {
+            return <tr>
+              {headerGroup.headers.map(header => {
+                return (
+                  <th {...header.getHeaderProps(header.getSortByToggleProps())}>
+                    {header.render('Header')}
+                    <span>
+                      {header.isSorted
+                        ? header.isSortedDesc
+                          ? 'V'
+                          : 'A'
+                        : ''}
+                    </span>
+                  </th>
+                );
+              })}
+            </tr>
+          })}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row, i) => {
+            prepareRow(row);
             return (
-              <th {...header.getHeaderProps(header.getSortByToggleProps())}>
-                {header.render('Header')}
-                <span>
-                  {header.isSorted
-                    ? header.isSortedDesc
-                      ? 'V'
-                      : 'A'
-                    : ''}
-                </span>
-              </th>
-            );
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <td {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
           })}
-        </tr>
-      })}
-    </thead>
-    <tbody>
-      {rows.map(row => {
-        prepareRow(row);
-        return <tr>
-          {row.cells.map((cell) => {
-            return <td>{cell.render('Cell')}</td>
-          })}
-        </tr>
-      })}
-    </tbody>
-  </table>);
+        </tbody>
+      </table>
+      <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>{'<<'}</button>
+      <button type='submit' onClick={() => previousPage()}> Previous </button>
+      <button type='submit' onClick={() => nextPage()}> Next </button>
+      <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>{'>>'}</button>
+      <br />
+      <br />
+      <span>
+        Page{' '}
+        <strong>
+          {pageIndex + 1} of {pageOptions.length}
+        </strong>{' '}
+      </span>
+      <br />
+      <br />
+
+      <select
+        value={pageSize}
+        onChange={(e) => setPageSize(Number(e.target.value))}
+      >
+        {[5, 10, 15, 20, 25].map(pageSize => (
+          <option key={pageSize}
+            value={pageSize}>
+            Show {pageSize}
+          </option>
+        ))}
+      </select>
+    </>
+  );
 };
 
 export default Table;
